c3e646135bccec2d105e2fe37e535842c3884df2
Marlena <sosnowska.marlena@wp.pl>
2023-09-28 22:24:10 +0200

    jwt token

--- src/main/java/com/example/restapi/config/RestAuthenticationSuccessHandler.java
+++ src/main/java/com/example/restapi/config/RestAuthenticationSuccessHandler.java
@@ -1,22 +1,44 @@
 package com.example.restapi.config;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.Date;
+
 
 @Component
 public class RestAuthenticationSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
 
+    private final long expirationTime;
+    private final String secret;
+
+    public RestAuthenticationSuccessHandler(
+            @Value("${jwt.expirationTime}") long expirationTime,
+            @Value("${jwt.secret}")String secret) {
+        this.expirationTime = expirationTime;
+        this.secret = secret;
+    }
+
+
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
                                         Authentication authentication) throws IOException, ServletException {
-        clearAuthenticationAttributes(request);
+        UserDetails principal = (UserDetails) authentication.getPrincipal();
+        String token = JWT.create()
+                .withSubject(principal.getUsername())
+                .withExpiresAt(new Date(System.currentTimeMillis() + expirationTime))
+                .sign(Algorithm.HMAC256(secret));
+        response.addHeader("Authorization", "Bearer " + token);
     }
 
 }
--- src/main/java/com/example/restapi/config/SecurityConfig.java
+++ src/main/java/com/example/restapi/config/SecurityConfig.java
@@ -5,11 +5,13 @@ import javax.sql.DataSource;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -25,7 +27,6 @@ import static org.springframework.security.config.Customizer.withDefaults;
 
 
 @Configuration
-@RequiredArgsConstructor
 @EnableWebSecurity
 public class SecurityConfig {
 
@@ -33,13 +34,24 @@ public class SecurityConfig {
     private final ObjectMapper objectMapper;
     private final RestAuthenticationSuccessHandler successHandler;
     private final RestAuthenticationFailureHandler failureHandler;
+    private final String secret;
+
+    public SecurityConfig(DataSource dataSource, ObjectMapper objectMapper, RestAuthenticationSuccessHandler successHandler,
+                          RestAuthenticationFailureHandler failureHandler,
+                          @Value("${jwt.secret}") String secret) {
+        this.dataSource = dataSource;
+        this.objectMapper = objectMapper;
+        this.successHandler = successHandler;
+        this.failureHandler = failureHandler;
+        this.secret = secret;
+    }
 
 
 
     @Bean
-    public InMemoryUserDetailsManager userDetailsService(PasswordEncoder passwordEncoder) {
+    public InMemoryUserDetailsManager userDetailsService() {
         UserDetails user = User.withUsername("test")
-                .password(passwordEncoder.encode("test"))
+                .password("test")
                 .roles("USER")
                 .build();
         return new InMemoryUserDetailsManager(user);
@@ -56,8 +68,11 @@ public class SecurityConfig {
                         .requestMatchers(antMatcher("/h2-console")).permitAll()
                         .anyRequest().authenticated()
                 )
+                .sessionManagement((session) -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .addFilter(authenticationFilter())
-                .exceptionHandling((exceptionalHandling) -> exceptionalHandling.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
+                .addFilter(new JwtAuthorizationFilter(authenticationManager(), userDetailsService(), secret))
+                //.exceptionHandling((exceptionalHandling) -> exceptionalHandling.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
                 .httpBasic(withDefaults())
                 .formLogin(withDefaults());
 
